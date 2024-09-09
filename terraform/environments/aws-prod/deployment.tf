3eac6b341400c307521961bc759c47a301b196ee
Edward Paget <ed.paget@gmail.com>
2024-09-08 23:12:01 -0500

    terraform: create github oidc deployment role

---
+++ terraform/environments/aws-prod/deployment.tf
@@ -0,0 +1,133 @@
+locals {
+  provider_url = "token.actions.githubusercontent.com"
+  audience     = "sts.amazonaws.com"
+  subject      = "repo:edpaget/dice-roller-bot"
+
+  account_id = data.aws_caller_identity.current.account_id
+}
+
+data "aws_iam_policy_document" "dice_roller_github_oidc_assume_policy" {
+  statement {
+    sid    = "GithubOidcAuth"
+    effect = "Allow"
+    actions = [
+      "sts:TagSession",
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${local.account_id}:oidc-provider/${local.provider_url}"]
+    }
+
+    condition {
+      test     = "ForAllValues:StringEquals"
+      variable = "${local.provider_url}:iss"
+      values   = ["https://${local.provider_url}"]
+    }
+
+    condition {
+      test     = "ForAllValues:StringEquals"
+      variable = "${local.provider_url}:aud"
+      values   = [local.audience]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "${local.provider_url}:sub"
+      values   = [local.subject]
+    }
+  }
+}
+
+resource "aws_iam_role" "dice_roller_github_oidc_deployment_role" {
+  name        = "dice-roller-github-oidc-deployment-role"
+  description = "role for github oidc ecr/ecs deployment actions for dice-roller"
+
+  assume_role_policy = data.aws_iam_policy_document.dice_roller_github_oidc_assume_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "dice_roller_ecr_deployment_policy_attachment" {
+  role       = aws_iam_role.dice_roller_github_oidc_deployment_role.name
+  policy_arn = aws_iam_policy.dice_roller_ecr_deployment_policy.arn
+}
+
+resource "aws_iam_policy" "dice_roller_ecr_deployment_policy" {
+  name        = "dice-roller-ecr-deployment-policy"
+  description = "allow ecr deployment by github oidc"
+
+  policy = data.aws_iam_policy_document.dice_roller_ecr_deployment_policy_doc.json
+
+}
+
+data "aws_iam_policy_document" "dice_roller_ecr_deployment_policy_doc" {
+  statement {
+    sid    = "ECRImagePush"
+    effect = "Allow"
+    actions = [
+      "ecr:CompleteLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+    ]
+    resources = [data.aws_ecr_repository.dice_roller.arn]
+  }
+
+  statement {
+    sid    = "AllowECRLogin"
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "dice_roller_ecs_deployment_policy_attachment" {
+  role       = aws_iam_role.dice_roller_github_oidc_deployment_role.name
+  policy_arn = aws_iam_policy.dice_roller_ecs_deployment_policy.arn
+}
+
+resource "aws_iam_policy" "dice_roller_ecs_deployment_policy" {
+  name        = "dice-roller-ecs-deployment-policy"
+  description = "allow ecs deployment by github oidc"
+
+  policy = data.aws_iam_policy_document.dice_roller_ecs_deployment_policy_doc.json
+
+}
+
+data "aws_iam_policy_document" "dice_roller_ecs_deployment_policy_doc" {
+  statement {
+    sid    = "RegisterTaskDefinition"
+    effect = "Allow"
+    actions = [
+      "ecs:RegisterTaskDefinition"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "PassRolesInTaskDefinition"
+    effect = "Allow"
+    actions = [
+      "iam:PassRole"
+    ]
+    resources = [
+      "arn:aws:iam::311245061868:role/ecsTaskExecutionRole",
+      "arn:aws:iam::311245061868:role/prod-dicer-roller-iam-role",
+    ]
+  }
+
+  statement {
+    sid    = "DeployService"
+    effect = "Allow"
+    actions = [
+      "ecs:UpdateService",
+      "ecs:DescribeServices"
+    ]
+    resources = [
+      aws_ecs_service.dice_roller.id
+    ]
+  }
+}
--- terraform/environments/aws-prod/ecs.tf
+++ terraform/environments/aws-prod/ecs.tf
@@ -1,37 +1,33 @@
-data "aws_region" "current" {}
-data "aws_partition" "current" {}
-data "aws_caller_identity" "current" {}
-
 resource "aws_ecs_cluster" "prod_dice_roller" {
   name = "prod-dice-roller-ecs-cluster"
 }
 
 resource "aws_ecs_task_definition" "dice_roller_task" {
-  family = "dice-roller-ecs-task-definition"
+  family       = "dice-roller-ecs-task-definition"
   track_latest = true
 
   container_definitions = jsonencode([{
-    name = "dice-roller-container"
+    name      = "dice-roller-container"
     essential = true
-    image = "${data.aws_ecr_repository.dice_roller.repository_url}:main"
+    image     = "${data.aws_ecr_repository.dice_roller.repository_url}:main"
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        awslogs-group = "/prod/dice-roller"
-        awslogs-region = "us-east-2"
+        awslogs-group         = "/prod/dice-roller"
+        awslogs-region        = "us-east-2"
         awslogs-stream-prefix = "ecs"
       }
     }
     secrets = [
       {
-        name = "DISCORD_TOKEN"
+        name      = "DISCORD_TOKEN"
         valueFrom = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/prod.discord_bot_token"
       }
     ]
   }])
 
-  cpu = 256
-  memory = 512
+  cpu          = 256
+  memory       = 512
   network_mode = "awsvpc"
 
   requires_compatibilities = [
@@ -39,7 +35,7 @@ resource "aws_ecs_task_definition" "dice_roller_task" {
   ]
 
   execution_role_arn = "arn:aws:iam::311245061868:role/ecsTaskExecutionRole"
-  task_role_arn = "arn:aws:iam::311245061868:role/prod-dicer-roller-iam-role"
+  task_role_arn      = "arn:aws:iam::311245061868:role/prod-dicer-roller-iam-role"
 
 }
 
@@ -51,8 +47,8 @@ resource "aws_ecs_service" "dice_roller" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets = data.aws_subnets.vpc_subnets.ids
-    security_groups = [aws_security_group.dice_roller.id]
+    subnets          = data.aws_subnets.vpc_subnets.ids
+    security_groups  = [aws_security_group.dice_roller.id]
     assign_public_ip = true
   }
 }
